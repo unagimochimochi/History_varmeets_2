9802e6a6f385c82719157667d30b15d21ebcfabe
ﾓｯﾁｰｻｧﾝ <67029618+unagimochimochi@users.noreply.github.com>
2020-10-02 10:23:51 +0900

    20201002: 友だちを取得し、UITableViewに表示

--- FriendsViewController.swift
+++ FriendsViewController.swift
@@ -7,52 +7,119 @@
 // Label受け渡し参考 https://qiita.com/azuma317/items/6b800bfca423e8fe2cf6
 
 import UIKit
+import CloudKit
+
+protocol CountObserver {
+    func didChange(newCount :Int)
+}
 
 class FriendsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var friendsTableView: UITableView!
     
-    var selectedImage: UIImage?
     var giveName: String = ""
     var giveID: String = ""
     var giveBio: String = ""
     
-    // section毎の画像配列
-    let imgArray: NSArray = ["Toshihiro","Yuka"]
+    let publicDatabase = CKContainer.default().publicCloudDatabase
     
-    let label1Array: [String] = ["利洋","優香"]
-    let label2Array: [String] = ["@toshihiro_sanada","@yuyuyuyuyukakka"]
-    let label3Array: [String] = ["SUACデザイン2年。ラーメン！！！","パンケーキ食べる人、誘ってください"]
+    var friendIDs: [String] = [] {
+        // 配列が更新されたら処理
+        didSet {
+            fetchFriendInfo()
+            // 1秒後に処理
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.friendsTableView.reloadData()
+            }
+        }
+    }
+
+    var friendNames = [String]()
+    var friendBios = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        friendIDs.removeAll()
+        friendNames.removeAll()
+        friendBios.removeAll()
+        
+        fetchFriends()
+    }
+    
+    func fetchFriends() {
+        
+        // 自分のレコードから友だち一覧を取得
+        let recordID1 = CKRecord.ID(recordName: "accountID-\(myID!)")
+        
+        publicDatabase.fetch(withRecordID: recordID1, completionHandler: {(record, error) in
+            
+            if let error = error {
+                print("友だち一覧取得エラー: \(error)")
+                return
+            }
+            
+            if let friendIDs = record?.value(forKey: "friends") as? [String] {
+                for friendID in friendIDs {
+                    print("友だち一覧取得成功")
+                    self.friendIDs.append(friendID)
+                }
+            }
+        })
+    }
+    
+    func fetchFriendInfo() {
+        
+        var count = 0
+        while count < friendIDs.count {
+            
+            let recordID2 = CKRecord.ID(recordName: "accountID-\(friendIDs[count])")
+            
+            publicDatabase.fetch(withRecordID: recordID2, completionHandler: {(record, error) in
+                
+                if let error = error {
+                    print("友だちの情報取得エラー: \(error)")
+                    return
+                }
+                
+                if let name = record?.value(forKey: "accountName") as? String {
+                    self.friendNames.append(name)
+                }
+                
+                if let bio = record?.value(forKey: "accountBio") as? String {
+                    self.friendBios.append(bio)
+                } else {
+                    self.friendBios.append("自己紹介が未入力です")
+                }
+            })
+            
+            count += 1
+        }
+    }
+    
     func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imgArray.count
+        return friendIDs.count
     }
     
     func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // tableCell の ID で UITableViewCell のインスタンスを生成
         let cell = table.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath)
-        let img = UIImage(named: imgArray[indexPath.row] as! String)
         
-        // Tag番号 1 で UIImageView インスタンスの生成
-        let imageView = cell.viewWithTag(1) as! UIImageView
-        imageView.image = img
-        imageView.layer.borderColor = UIColor.gray.cgColor // 枠線の色
-        imageView.layer.borderWidth = 0.5 // 枠線の太さ
-        imageView.layer.cornerRadius = imageView.bounds.width / 2 // 丸くする
-        imageView.layer.masksToBounds = true // 丸の外側を消す
+        let icon = cell.viewWithTag(1) as! UIImageView
+        icon.layer.borderColor = UIColor.gray.cgColor // 枠線の色
+        icon.layer.borderWidth = 0.5 // 枠線の太さ
+        icon.layer.cornerRadius = icon.bounds.width / 2 // 丸くする
+        icon.layer.masksToBounds = true // 丸の外側を消す
         
-        // Tag番号 ２ で UILabel インスタンスの生成
-        let label1 = cell.viewWithTag(2) as! UILabel
-        label1.text = String(describing: label1Array[indexPath.row])
+        let nameLabel = cell.viewWithTag(2) as! UILabel
+        nameLabel.text = friendNames[indexPath.row]
         
-        // Tag番号 ３ で UILabel インスタンスの生成
-        let label2 = cell.viewWithTag(3) as! UILabel
-        label2.text = String(describing: label2Array[indexPath.row])
+        let idLabel = cell.viewWithTag(3) as! UILabel
+        idLabel.text = friendIDs[indexPath.row]
         
         return cell
     }
@@ -64,25 +131,20 @@ class FriendsViewController: UIViewController, UITableViewDataSource, UITableVie
     
     func tableView(_ table: UITableView, didSelectRowAt indexPath: IndexPath) {
         table.deselectRow(at: indexPath, animated: true)
-        // [indexPath.row] から画像名を探し、UImage を設定
-        selectedImage = UIImage(named: imgArray[indexPath.row] as! String)
-        self.giveName = self.label1Array[indexPath.item]
-        self.giveID = self.label2Array[indexPath.item]
-        self.giveBio = self.label3Array[indexPath.item]
-        if selectedImage != nil {
-            performSegue(withIdentifier: "toFriendProfileVC", sender: nil)
-        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "toFriendProfileVC" {
+            
             let fpVC = segue.destination as! FriendProfileViewController
-            // SubViewController のselectedImgに選択された画像を設定する
-            fpVC.selectedImg = selectedImage
-            fpVC.receiveName = giveName
-            fpVC.receiveID = giveID
-            fpVC.receiveBio = giveBio
+
+            if let selectedIndexPath = friendsTableView.indexPathForSelectedRow {
+                
+                fpVC.receiveName = friendNames[selectedIndexPath.row]
+                fpVC.receiveID = friendIDs[selectedIndexPath.row]
+                fpVC.receiveBio = friendBios[selectedIndexPath.row]
+            }
         }
     }
 
