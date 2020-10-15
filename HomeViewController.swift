baa20351b58033d9085ae2f503c7adc67e863966
ﾓｯﾁｰｻｧﾝ <67029618+unagimochimochi@users.noreply.github.com>
2020-10-15 10:16:00 +0900

    20201015: 起動時にデータベースから予定を取得
    取得とUserDefaultsへの保存はうまくいっているが、TableViewの更新に失敗してる？

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -22,6 +22,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var dateAndTimes = [String]()
     var planTitles = [String]()
+    var participantIDs = [String]()    // 起動時にデータベースから取得
     var participantNames = [String]()
     var numberOfParticipants = [Int]()
     var places = [String]()
@@ -31,13 +32,14 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     let publicDatabase = CKContainer.default().publicCloudDatabase
     var planIDsOnDatabase = [[String]]()
     
-    var fetchedRequest = [String]()
-    var friendIDs = [String]() // 起動時の友だち一覧
-    var friendIDsToMe = [String]() // friendIDs配列に申請許可者を追加した一覧
+    var fetchedRequests = ["NO", "NO", "NO"]
+    var friendIDs = [String]()    // 起動時の友だち一覧
+    var friendIDsToMe = [String]()    // friendIDs配列に申請許可者を追加した一覧
     
     var timer: Timer!
     
-    var addPlanIDCheckCount = 0
+    var fetchPlansTimer: Timer!
+    var fetchPlansCheck = [Bool]()
     
     @IBOutlet weak var planTable: UITableView!
 
@@ -271,7 +273,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                 
             } else {
                 participantNames.append(rep)
-                numberOfParticipants.append(number)
+                numberOfParticipants.append(number + 1)
             }
             
             userDefaults.set(participantNames, forKey: "ParticipantNames")
@@ -444,24 +446,87 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         // 1秒後に処理
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             // 友だち申請されている場合
-            if self.fetchedRequest.isEmpty == false {
+            if self.fetchedRequests.isEmpty == false {
                 self.performSegue(withIdentifier: "toRequestedVC", sender: nil)
             }
         }
         
         if userDefaults.object(forKey: "myID") != nil {
             myID = userDefaults.string(forKey: "myID")
-            print(myID!)
+            print("myID: \(myID!)")
             
             // 友だち申請をデータベースから取得
-            fetchRequest(id: myID!)
+            fetchRequests(id: myID!)
+            
             // 友だち一覧をデータベースから取得
             fetchFriendIDs(id: myID!)
+            
+            // 予定一覧取得監視タイマースタート
+            fetchPlansTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(completedFetchingPlans), userInfo: nil, repeats: true)
+            
+            // 予定一覧をデータベースから取得
+            fetchMyPlanIDs(completion: {
+                
+                // ID取得が終わったら他の配列に適当な初期値を代入
+                self.dateAndTimes.removeAll()
+                estimatedTimes.removeAll()
+                self.planTitles.removeAll()
+                self.participantIDs.removeAll()
+                self.participantNames.removeAll()
+                self.numberOfParticipants.removeAll()
+                self.places.removeAll()
+                self.lats.removeAll()
+                self.lons.removeAll()
+                
+                for _ in 0...(myPlanIDs.count - 1) {
+                    self.dateAndTimes.append("日時")
+                    estimatedTimes.append(Date(timeIntervalSinceReferenceDate: 0.0))
+                    self.planTitles.append("予定サンプル")
+                    self.participantIDs.append("participantID")
+                    self.participantNames.append("参加者")
+                    self.numberOfParticipants.append(0)
+                    self.places.append("場所")
+                    self.lats.append("緯度")
+                    self.lons.append("経度")
+                    self.fetchPlansCheck.append(false)
+                }
+                
+                // 予定の詳細を取得
+                for i in 0...(myPlanIDs.count - 1) {
+                    self.fetchMyPlanDetails(index: i, completion: {
+                        // estimatedTimeから文字列に
+                        let formatter = DateFormatter()
+                        formatter.timeStyle = .short
+                        formatter.dateStyle = .full
+                        formatter.timeZone = NSTimeZone.local
+                        formatter.locale = Locale(identifier: "ja_JP")
+                        self.dateAndTimes[i] = formatter.string(from: estimatedTimes[i])
+                        
+                        // 参加代表者のIDから名前を取得
+                        self.fetchParticipantName(index: i, completion: {
+                            
+                            // 完了チェック
+                            self.fetchPlansCheck[i] = true
+                            
+                            // UserDefaultsに保存
+                            userDefaults.set(myPlanIDs, forKey: "PlanIDs")
+                            userDefaults.set(self.dateAndTimes, forKey: "DateAndTimes")
+                            userDefaults.set(estimatedTimes, forKey: "EstimatedTimes")
+                            userDefaults.set(self.planTitles, forKey: "PlanTitles")
+                            userDefaults.set(self.participantNames, forKey: "ParticipantNames")
+                            userDefaults.set(self.numberOfParticipants, forKey: "NumberOfParticipants")
+                            userDefaults.set(self.places, forKey: "Places")
+                            userDefaults.set(self.lats, forKey: "lats")
+                            userDefaults.set(self.lons, forKey: "lons")
+                        })
+                    })
+                }
+            })
         }
         
         if userDefaults.object(forKey: "myName") != nil {
             myName = userDefaults.string(forKey: "myName")
-            print(myName!)
+            print("myName: \(myName!)")
         }
         
         if userDefaults.object(forKey: "PlanIDs") != nil {
@@ -741,7 +806,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         planTable.reloadData()
     }
     
-    func fetchRequest(id: String) {
+    func fetchRequests(id: String) {
         
         let recordID = CKRecord.ID(recordName: "accountID-\(id)")
         
@@ -753,23 +818,22 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
             
             if let request01 = record?.value(forKey: "requestedAccountID_01") as? String {
-                self.fetchedRequest.append(request01)
+                self.fetchedRequests[0] = request01
             }
             if let request02 = record?.value(forKey: "requestedAccountID_02") as? String {
-                self.fetchedRequest.append(request02)
+                self.fetchedRequests[1] = request02
             }
             if let request03 = record?.value(forKey: "requestedAccountID_03") as? String {
-                self.fetchedRequest.append(request03)
+                self.fetchedRequests[2] = request03
             }
-            print(self.fetchedRequest)
+            print("友だち申請: \(self.fetchedRequests)")
             
             // 申請者のIDのみ配列に残す
-            while self.fetchedRequest.contains("NO") {
-                if let index = self.fetchedRequest.index(of: "NO") {
-                    self.fetchedRequest.remove(at: index)
+            while self.fetchedRequests.contains("NO") {
+                if let index = self.fetchedRequests.index(of: "NO") {
+                    self.fetchedRequests.remove(at: index)
                 }
             }
-            print(self.fetchedRequest)
         })
     }
     
@@ -788,11 +852,130 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                 for friendID in friendIDs {
                     self.friendIDs.append(friendID)
                 }
-                print(self.friendIDs)
+                print("友だち: \(self.friendIDs)")
             } else {
                 print("友だち0人")
             }
         })
+    }
+    
+    func fetchMyPlanIDs(completion: @escaping () -> ()) {
+        
+        let recordID = CKRecord.ID(recordName: "accountID-\(myID!)")
+        
+        publicDatabase.fetch(withRecordID: recordID, completionHandler: {(record, error) in
+            
+            if let error = error {
+                print("自分の予定一覧取得エラー: \(error)")
+                return
+            }
+            
+            if let planIDs = record?.value(forKey: "planIDs") as? [String] {
+                
+                myPlanIDs.removeAll()
+                
+                for myPlanID in planIDs {
+                    myPlanIDs.append(myPlanID)
+                }
+                
+                completion()
+                
+            } else {
+                print("データベースの予定なし")
+            }
+        })
+    }
+    
+    func fetchMyPlanDetails(index: Int, completion: @escaping () -> ()) {
+        
+        let recordID = CKRecord.ID(recordName: "planID-\(myPlanIDs[index])")
+        
+        publicDatabase.fetch(withRecordID: recordID, completionHandler: {(record, error) in
+            
+            if let error = error {
+                print("自分の\(index)番目の予定取得エラー: \(error)")
+                return
+            }
+            
+            if let estimatedTime = record?.value(forKey: "estimatedTime") as? Date {
+                estimatedTimes[index] = estimatedTime
+            }
+            
+            if let planTitle = record?.value(forKey: "planTitle") as? String {
+                self.planTitles[index] = planTitle
+            }
+            
+            var everyone = [String]()
+            
+            if let authorID = record?.value(forKey: "authorID") as? String {
+                everyone.append(authorID)
+            }
+            
+            if let participantIDs = record?.value(forKey: "participantIDs") as? [String] {
+                for participantID in participantIDs {
+                    everyone.append(participantID)
+                }
+            }
+            
+            self.numberOfParticipants[index] = everyone.count
+            
+            if let myIndex = everyone.index(of: myID!) {
+                everyone.remove(at: myIndex)
+                self.participantIDs[index] = everyone[0]    // 代表参加者（0番目）のID
+            }
+            
+            // 参加者に自分がいないとき（予定サンプル）
+            else {
+                self.participantIDs[index] = everyone[0]
+            }
+            
+            if let place = record?.value(forKey: "placeName") as? String {
+                self.places[index] = place
+            }
+            
+            if let location = record?.value(forKey: "placeLatAndLon") as? CLLocation {
+                self.lats[index] = location.coordinate.latitude.description
+                self.lons[index] = location.coordinate.longitude.description
+            }
+            
+            completion()
+        })
+    }
+    
+    func fetchParticipantName(index: Int, completion: @escaping () -> ()) {
+        
+        let recordID = CKRecord.ID(recordName: "accountID-\(participantIDs[index])")
+        
+        publicDatabase.fetch(withRecordID: recordID, completionHandler: {(record, error) in
+            
+            if let error = error {
+                print("参加者（\(self.participantIDs[index])）の名前取得エラー: \(error)")
+                return
+            }
+            
+            if let participantName = record?.value(forKey: "accountName") as? String {
+                self.participantNames[index] = participantName
+            }
+            
+            completion()
+        })
+    }
+    
+    @objc func completedFetchingPlans() {
+        print("fetchingPlans")
+        
+        if fetchPlansCheck.contains(false) == false {
+            
+            print("completedFetchingPlans!")
+            
+            // タイマーを止める
+            if let workingTimer = fetchPlansTimer {
+                workingTimer.invalidate()
+            }
+            
+            // UI更新
+            planTable.reloadData()
+        }
     }
     
     func fetchPlanIDs(accountID: String, index: Int, completion: @escaping () -> ()) {
@@ -954,7 +1137,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         if identifier == "toRequestedVC" {
             let requestedVC = segue.destination as! RequestedViewController
-            requestedVC.requestedIDs = self.fetchedRequest
+            requestedVC.requestedIDs = self.fetchedRequests
         }
     }
     
