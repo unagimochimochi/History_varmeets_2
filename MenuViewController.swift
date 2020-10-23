5c23d2bfd51f57fd56713d634889c890393881a9
ﾓｯﾁｰｻｧﾝ <67029618+unagimochimochi@users.noreply.github.com>
2020-10-23 11:00:26 +0900

    20201023: メニューに友だちとお気に入りの数を表示
    お気に入りのUserDefaultsの読み込みをお気に入りタブを開いたときにしているので、いきなりホーム画面でメニューを開くと0になる。

--- MenuViewController.swift
+++ MenuViewController.swift
@@ -16,14 +16,19 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idLabel: UILabel!
     
+    @IBOutlet weak var numberOfFriendsButton: UIButton!
+    @IBOutlet weak var numberOfFavsButton: UIButton!
+    
     @IBOutlet weak var bioLabel: UILabel!
+    
+    var numberOfFriends: Int?
     
     let publicDatabase = CKContainer.default().publicCloudDatabase
     
     var fetchedBio: String?
     
-    var timer: Timer!
-    var check = 0
+    var fetchingBioTimer: Timer!
+    var fetchingBioCheck = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +41,7 @@ class MenuViewController: UIViewController {
         fetchMyBio()
         
         // タイマースタート
-        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(fetchingBio), userInfo: nil, repeats: true)
+        fetchingBioTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(fetchingBio), userInfo: nil, repeats: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -45,6 +50,18 @@ class MenuViewController: UIViewController {
         if let id = myID, let name = myName {
             idLabel.text = id
             nameLabel.text = name
+        }
+        
+        if let numberOfFriends = self.numberOfFriends {
+            numberOfFriendsButton.setTitle(numberOfFriends.description, for: .normal)
+        }
+        
+        if favPlaces.isEmpty == false {
+            if favPlaces[0] == "お気に入り" {
+                numberOfFavsButton.setTitle((favPlaces.count - 1).description, for: .normal)
+            } else {
+                numberOfFavsButton.setTitle(favPlaces.count.description, for: .normal)
+            }
         }
     }
     
@@ -94,10 +111,10 @@ class MenuViewController: UIViewController {
             
             if let bio = record?.value(forKey: "accountBio") as? String {
                 self.fetchedBio = bio
-                self.check = 1
+                self.fetchingBioCheck = 1
             } else {
                 print("クラウドのBioが空")
-                self.check = 2
+                self.fetchingBioCheck = 2
             }
         })
     }
@@ -105,21 +122,21 @@ class MenuViewController: UIViewController {
     @objc func fetchingBio() {
         print("fetchingBio")
         
-        if check != 0 {
+        if fetchingBioCheck != 0 {
             print("Completed fetching my bio!")
             
             // タイマーを止める
-            if let workingTimer = timer {
+            if let workingTimer = fetchingBioTimer {
                 workingTimer.invalidate()
             }
             
-            if check == 1 {
+            if fetchingBioCheck == 1 {
                 // bioを表示
                 bioLabel.text = fetchedBio
                 bioLabel.textColor = .black
             }
             
-            else if check == 2 {
+            else if fetchingBioCheck == 2 {
                 // bioが空であることを表示
                 bioLabel.text = "自己紹介が未入力です"
                 bioLabel.textColor = .systemGray
